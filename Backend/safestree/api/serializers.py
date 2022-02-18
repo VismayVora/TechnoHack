8b9d61bd8e18e30c73abdf9e6e8a8883c5477a5e
Vismay Vora <voravismay9@gmail.com>
2022-02-18 21:47:29 +0530

    Created new dev branch and added files in Frontend and Backend folder

---
+++ Backend/safestree/api/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import MyUser
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+	password=serializers.CharField(max_length=32,min_length=8,write_only = True)
+	
+	class Meta:
+		model = MyUser
+		fields = ['email','password','first_name','last_name','phone_no','address']
+		
+	def validate(self,attrs):
+		phone_no = attrs.get('phone_no',' ')
+		if not phone_no:
+			raise serializers.ValidationError('Phone number is compulsory')
+		return attrs
+		
+	def create(self,validated_data):
+		return MyUser.objects.create_user(**validated_data)
+
+class LoginSerializer(serializers.ModelSerializer):
+	password=serializers.CharField(max_length=32,min_length=8,write_only = True)
+	
+	class Meta:
+		model = MyUser
+		fields = ['phone_no','password']
+        
+	def validate(self,attrs):
+		phone_no = attrs.get('phone_no',' ')
+		if not phone_no:
+			raise serializers.ValidationError('Phone number is compulsory')
+		return attrs
+
+    
+
+		
