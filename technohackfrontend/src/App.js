00bfa2f6c15473d31a7686d304436b070ddfb3b4
harsh0989 <harshmangukiya1506@gmail.com>
2022-02-18 19:18:50 +0530

    Routing, created splash page

--- technohackfrontend/src/App.js
+++ technohackfrontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
-
+import Splashpage from './pages/Splashpage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Splashpage />} />
+        <Route exact path='/login' element={<Login />} />
+      </Routes>
+    </Router>
+  )
+
 }
 
 export default App;
