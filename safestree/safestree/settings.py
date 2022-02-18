7e8ae016f206c298f3d3350d62e34e8ce42295df
Vismay Vora <voravismay9@gmail.com>
2022-02-19 05:21:52 +0530

    CChanged default permission to AllowAny from IsAuthenticated

--- safestree/safestree/settings.py
+++ safestree/safestree/settings.py
@@ -122,8 +122,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
+        'rest_framework.permissions.AllowAny',
+]
 }
 
 
