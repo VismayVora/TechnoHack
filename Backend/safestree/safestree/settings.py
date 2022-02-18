ac95366f8edd7b113493167e1532f64d5794b44a
Vismay Vora <voravismay9@gmail.com>
2022-02-19 03:00:33 +0530

    Updated settings.py

--- Backend/safestree/safestree/settings.py
+++ Backend/safestree/safestree/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+import django_heroku
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_04ybgi7nu#=^v@8r!==3jh4i&!x4(2rmp0coh6t!e4)372l%)'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
